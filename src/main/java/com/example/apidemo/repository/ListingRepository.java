3f63d675449d5b2198bca993c831d7711ba739b3
Mathieu <langevin36@hotmail.com>
2021-06-15 22:00:34 -0400

    Fixed postalcode search to be more flexible and include given parameter without the "_"

--- src/main/java/com/example/apidemo/repository/ListingRepository.java
+++ src/main/java/com/example/apidemo/repository/ListingRepository.java
@@ -14,6 +14,6 @@ import java.util.UUID;
 @Repository
 public interface ListingRepository extends JpaRepository<Listing, UUID> {
 
-    @Query("SELECT l FROM Listing l WHERE l.price > ?1 and l.price < ?2 and l.nb_beds > ?3 and l.postalCode LIKE ?4")
+    @Query("SELECT l FROM Listing l WHERE l.price > ?1 and l.price < ?2 and l.nb_beds > ?3 and l.postalCode LIKE %?4%")
     List<ListingBasicDetails> getAllListings(Integer min_Price, Integer max_Price, Integer nb_beds, String postalCode);
 }
